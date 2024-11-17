04be3d66d46406bcf53a04d50b6bebed251dd4f8
tensanbaby <64396847+tensanbaby@users.noreply.github.com>
2024-11-18 03:36:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"  # Adjust the version as needed
+    }
+  }
+
+  required_version = ">= 1.3.0"  # Adjust based on your Terraform CLI version
+}
+
+provider "aws" {
+  region = "us-east-1"  # Specify your AWS region
+}
+
+
+
+
 # IAM Role for the EKS Cluster
 resource "aws_iam_role" "cluster-role" {
   name = "cluster-role"
