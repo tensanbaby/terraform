b3f98bc657b4a97ff4eaca141785522ee7d9bb49
tensanbaby <64396847+tensanbaby@users.noreply.github.com>
2024-11-18 03:41:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
 
+
+provider "aws" {
+  region = "us-east-1"  # Specify your AWS region
+}
 
 
 
